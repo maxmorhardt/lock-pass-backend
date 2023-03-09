a21f21f4bc0142e32d944a3caaa4e96f54813cc8
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2023-03-09 13:48:16 -0500

    initial commit

---
+++ src/main/java/co/lockpass/lockpass/LockpassApplication.java
@@ -0,0 +1,13 @@
+package co.lockpass.lockpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockpassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockpassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/lockpass/lockpass/LockpassApplicationTests.java
@@ -0,0 +1,13 @@
+package co.lockpass.lockpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockpassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
